e8676040be056ad1ac51b0ce9570a78f5d8084aa
veena <veenagowda92@gmail.com>
2024-09-27 17:44:15 +0530

    main person

--- src/test/java/gitPracties/test1.java
+++ src/test/java/gitPracties/test1.java
@@ -4,6 +4,8 @@ public class test1 {
 	public void practiesMethod1()
 	{
 		System.out.println("Git practies1");
+		System.out.println("Git practies3");
+		System.out.println("Git practies4");
 	}
 
 
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -3,6 +3,8 @@ public class test {
 	public void practiesMethod()
 	{
 		System.out.println("Git practies");
+		System.out.println("Git practies1");
+		System.out.println("Git practies2");
 	}
 
 }
